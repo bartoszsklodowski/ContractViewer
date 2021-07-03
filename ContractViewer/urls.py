fe6d1e044021c1ecb84955226658b2450856c299
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-07-03 14:03:11 +0200

    Add accounts urls

--- ContractViewer/urls.py
+++ ContractViewer/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from contracts.views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
+    path('contracts/', include('contracts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
