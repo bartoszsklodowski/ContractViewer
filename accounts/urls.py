0c06b10ba2df6227223fd7a99aac5e42c8ab4d5d
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-07-04 09:56:35 +0200

    Fix01-add .as_view()

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from accounts.views import UserRegisterView
 app_name = "accounts"
 
 urlpatterns = [
-    path('registration/', UserRegisterView, name='registration'),
+    path('registration/', UserRegisterView.as_view(), name='registration'),
 ]
