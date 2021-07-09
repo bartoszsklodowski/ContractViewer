0d5b40685c04f55fb83a26e269ae7d8f4516d11c
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-07-10 00:08:34 +0200

    Register urls related to login, logout and dashboard.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from accounts.views import UserRegisterView
+from django.urls import path, include
+from accounts.views import UserRegisterView, dashboard, UserResetPasswordView
 
 app_name = "accounts"
 
 urlpatterns = [
+    path('dashboard/', dashboard, name='dashboard'),
+    path('login/dashboard/', dashboard, name='dashboard'),
+    path('logout/dashboard/', dashboard, name='dashboard'),
     path('registration/', UserRegisterView.as_view(), name='registration'),
 ]
