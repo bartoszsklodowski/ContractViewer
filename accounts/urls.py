23b7770ca688e08648cd32fe8b3d67c634fab7d9
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-08-04 22:45:08 +0200

    Change name of login and logout paths.

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
-    path('login/dashboard/', dashboard, name='dashboard'),
-    path('logout/dashboard/', dashboard, name='dashboard'),
+    path('login/dashboard/', dashboard, name='login'),
+    path('logout/dashboard/', dashboard, name='logout'),
     path('registration/', UserRegisterView.as_view(), name='registration'),
 ]
