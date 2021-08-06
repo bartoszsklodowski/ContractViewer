5e3ba819d5c07958d42b41077457fc8fcee8fc56
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-08-06 15:16:40 +0200

    Change DATABASE for Heroku.

--- ContractViewer/settings.py
+++ ContractViewer/settings.py
@@ -95,6 +95,7 @@ WSGI_APPLICATION = 'ContractViewer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
     }
 }
 
