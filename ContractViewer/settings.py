91dee0bff9a6c483a1e5957b77a12ed12756134a
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-08-06 15:19:35 +0200

    Change DATABASE for Heroku.

--- ContractViewer/settings.py
+++ ContractViewer/settings.py
@@ -96,6 +96,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
