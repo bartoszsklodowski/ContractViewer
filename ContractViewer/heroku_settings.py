e2c4a094464c6e34a148d72b17e7b519ecd598a5
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-08-06 15:44:08 +0200

    Change heroku_settings.

--- ContractViewer/heroku_settings.py
+++ ContractViewer/heroku_settings.py
@@ -1,5 +1,11 @@
 from .settings import *
 import dj_database_url
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'postgresql',
+        }
+    }
 
-DATABASES = { 'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
+DATABASES['default'] =  dj_database_url.config(conn_max_age=600, ssl_require=True)
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
