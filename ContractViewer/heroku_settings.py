affd0479c3231e9cf43ad1d4dee3c7500911a0c8
Bartosz Skłodowski <sklodowski94@gmail.com>
2021-08-06 15:33:26 +0200

    Create heroku_settings.

---
+++ ContractViewer/heroku_settings.py
@@ -0,0 +1,5 @@
+from .settings import *
+import dj_database_url
+
+DATABASES = { 'default' : dj_database_url.config(conn_max_age=600, ssl_require=True)}
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
